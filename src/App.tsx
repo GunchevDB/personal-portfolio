a7f3cfd95502b74e8616db46ba0f70bfc2c7870e
Dimitar Gunchev <dimitar.gunchev@logichainge.com>
2025-10-07 11:54:08 +0300

    Fix nextjs analytics to react oopsie :)

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ import ProjectModal from './components/ProjectModal';
 import { Project } from './components/ProjectCard';
 import CustomCursor from './components/CustomCursor';
 import Footer from './components/Footer';
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 gsap.registerPlugin(ScrollTrigger);
 
